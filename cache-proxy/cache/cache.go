b33921de131d96ae2c58e661f2c892ae6497c251
Konstantinos Amplianitis <kamplianitis@admin.grnet.gr>
2026-01-27 22:15:04 +0200

    feat: Cache basic implementation

--- cache-proxy/cache/cache.go
+++ cache-proxy/cache/cache.go
@@ -1,8 +1,12 @@
 package cache
 
 import (
+	"fmt"
 	"sync"
 )
+
+// thread-safety on
+const Threaded = true
 
 // we will check for that -> maybe it needs to be a json
 type Key string
@@ -11,5 +15,36 @@ type Value interface{}
 
 type Cache struct {
 	data map[Key]Value
-	lock sync.Mutex
+	lock sync.RWMutex // syncronization purposes
+}
+
+func (c *Cache) SetValueToCache(key Key, value Value) {
+	if Threaded {
+		c.lock.Lock()
+		defer c.lock.Unlock()
+	}
+	c.data[key] = value
+}
+
+func (c *Cache) GetCachedValue(key Key) (Value, error) {
+	if Threaded {
+		// since the data are not about to be edited, it is considered wiser to
+		// use RLock instead of the classic Lock.
+
+		c.lock.RLock()
+		defer c.lock.RUnlock()
+	}
+	value, exists := c.data[key]
+
+	if !exists {
+		return nil, fmt.Errorf("Key does not exist in cache")
+	}
+
+	return value, nil
+}
+
+func NewCache() *Cache {
+	return &Cache{
+		data: make(map[Key]Value),
+	}
 }
