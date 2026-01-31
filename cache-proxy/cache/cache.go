6efe42c4e12b4dc9cd09340b7a9420f739860798
Konstantinos Amplianitis <kamplianitis@admin.grnet.gr>
2026-01-31 22:27:12 +0200

    doc(cache): Add more comments to make the process understandable even further

--- cache-proxy/cache/cache.go
+++ cache-proxy/cache/cache.go
@@ -11,6 +11,8 @@ const Threaded = true
 // we will check for that -> maybe it needs to be a json
 type Key string
 
+// basically the "Any" type in GO -> This kind of approach
+// allows flexibility but makes it impossible to apply type safety
 type Value interface{}
 
 type Cache struct {
@@ -30,7 +32,6 @@ func (c *Cache) GetCachedValue(key Key) (Value, error) {
 	if Threaded {
 		// since the data are not about to be edited, it is considered wiser to
 		// use RLock instead of the classic Lock.
-
 		c.lock.RLock()
 		defer c.lock.RUnlock()
 	}
@@ -43,7 +44,19 @@ func (c *Cache) GetCachedValue(key Key) (Value, error) {
 	return value, nil
 }
 
+func (c *Cache) DropFromCache(key Key) {
+	// delete a Cache key,value
+	if Threaded {
+		c.lock.Lock()
+		defer c.lock.Unlock()
+	}
+	// delete is no-op in case of not
+	// finding the element so no need to take cases there.
+	delete(c.data, key)
+}
+
 func NewCache() *Cache {
+	// no need to initialize lock -> Go does it automatically to 0
 	return &Cache{
 		data: make(map[Key]Value),
 	}
